334a88495e632745ee1234350d7098a5ce8bc9b4
fak1r <fak1r@yandex.ru>
2024-01-07 18:41:44 +0300

    eslint, googleFonts, SASS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      Inter: [400, 700]
+    }
+  },
+  app: {
+    head: {
+      "title": "Shop",
+      "meta": [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        }
+      ],
+      "link": [],
+      "style": [],
+      "script": [],
+      "noscript": []
+    },
+  },
+  css: [
+    '~/assets/styles/main.scss'
+  ],
 })
